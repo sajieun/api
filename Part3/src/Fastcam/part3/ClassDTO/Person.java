32f634231b056f937b50f7f13427ca4d2db83ab0
sajieun <sajieun000721@naver.com>
2023-10-31 23:59:53 +0900

    part3 start chap.01

---
+++ Part3/src/Fastcam/part3/ClassDTO/Person.java
@@ -0,0 +1,18 @@
+package Fastcam.part3.ClassDTO;
+
+public class Person {
+    public int age;
+    public String name;
+    public String phone;
+
+    public void play(){
+        System.out.println("운동을 한다");
+    }
+    public void eat(){
+        System.out.println("음식을 먹다");
+    }
+    public void walk(){
+        System.out.println("걷다");
+    }
+}
+
