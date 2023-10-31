26e7c875b243ea2046a6fb035d4ade4ff95e8940
sajieun <sajieun000721@naver.com>
2023-11-01 01:24:16 +0900

    Person, PersonTest , this

--- Part3/src/Fastcam/part3/ClassDTO/Person.java
+++ Part3/src/Fastcam/part3/ClassDTO/Person.java
@@ -4,6 +4,12 @@ public class Person {
     public int age;
     public String name;
     public String phone;
+// 생성자 메서드라고 부른다 class이름 == Person 이랑 같기에
+    public Person(){
+        this.phone = "010-4444-4444";
+        this.age = 11;
+        this.name = "jeri";
+    }
 
     public void play(){
         System.out.println("운동을 한다");
